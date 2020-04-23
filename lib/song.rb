706bf324fdde1cee8e65045c96be2a45889aefe2
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-23 23:14:39 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,51 @@
+require 'pry'
+
+class Song
+  
+  attr_accessor :name, :artist, :genre
+
+
+  
+  @@artists = []
+  @@genres = []
+  @@count = 0
+  @@artist_count = {}
+  @@genre_count = {}
+  
+  def initilize(song_name, artist, genre)  
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+    @@count += 1
+  end
+
+
+  def self.count
+    @@count
+  end
+
+
+def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
+  end
+
+
+end
