f28b9676a3e3f12e83756d0e00cfd32c6ca4faf2
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-23 23:19:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,17 +37,6 @@ def self.artists
     genre_count
   end
 
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
 
   def self.artist_count
     @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
